14428e779e9a595de0fc395b90fea222d1d3aafa
yoda.ou <yoda.ou@tuya.com>
2022-08-25 14:11:17 +0800

    update api doc to 4.0

--- search/classes_f.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['region_2936',['Region',['../classcom_1_1tuya_1_1smart_1_1android_1_1user_1_1bean_1_1_region.html',1,'com::tuya::smart::android::user::bean']]],
-  ['roomauthbean_2937',['RoomAuthBean',['../classcom_1_1tuya_1_1smart_1_1home_1_1sdk_1_1bean_1_1_room_auth_bean.html',1,'com::tuya::smart::home::sdk::bean']]],
-  ['roombean_2938',['RoomBean',['../classcom_1_1tuya_1_1smart_1_1home_1_1sdk_1_1bean_1_1_room_bean.html',1,'com::tuya::smart::home::sdk::bean']]],
-  ['rule_2939',['Rule',['../classcom_1_1tuya_1_1smart_1_1home_1_1sdk_1_1bean_1_1scene_1_1condition_1_1rule_1_1_rule.html',1,'com::tuya::smart::home::sdk::bean::scene::condition::rule']]]
-];
